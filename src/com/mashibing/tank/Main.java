77a671a9de5e248c39b6cd6ab440b80ae2e0fa69
lihongyan435 <13866660195@163.com>
2024-06-13 18:15:39 +0800

    init project

---
+++ src/com/mashibing/tank/Main.java
@@ -0,0 +1,16 @@
+package com.mashibing.tank;
+
+/**
+ * 功能:
+ * 作者：黄焖鸡
+ * 日期：2024-06-13 16:14
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        TankFrame tf = new TankFrame();
+        while(true){
+            Thread.sleep(50);
+            tf.repaint();
+        }
+    }
+}
