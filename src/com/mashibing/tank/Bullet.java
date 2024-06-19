5c625ad5336369a59e4c53909450c9f48d8057cd
lihongyan435 <13866660195@163.com>
2024-06-19 16:47:38 +0800

    调整爆炸的位置

--- src/com/mashibing/tank/Bullet.java
+++ src/com/mashibing/tank/Bullet.java
@@ -80,7 +80,9 @@ public class Bullet {
         if(rect1.intersects(rect2)){ // 如果子弹和敌军相交，则子弹和敌军都死了
             tank.die();
             this.die();
-            tf.explodes.add(new Explode(x,y,tf));
+            int eX = tank.getX() + Tank.WIDTH/2 - Explode.WIDTH/2;
+            int eY = tank.getY() + Tank.HEIGHT/2 - Explode.HEIGHT/2;
+            tf.explodes.add(new Explode(eX,eY,tf));
         }
     }
     private void die(){
